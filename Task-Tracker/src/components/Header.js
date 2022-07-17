4bd77fe1c5c3dbd6f2a1cf37543fcd0bac107a2f
Andrew Ashraf <62217996+andrewashrafattia15@users.noreply.github.com>
2022-07-17 14:59:04 +0200

    Add files via upload

---
+++ Task-Tracker/src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAddTask }) => {
+  
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+export default Header;
